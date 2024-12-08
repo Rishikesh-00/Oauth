1cc6af0024b32b8e9bab89c71efd50aba246bb56
Rishikesh <keshrishi1789@gmail.com>
2024-12-08 23:16:48 +0530

    dashboard

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
-export default function Login() {
-    const { loginWithRedirect } = useAuth0();
-  return (
+import { useNavigate } from 'react-router-dom';
 
+export default function Login() {
+  const { loginWithRedirect } = useAuth0();
+  const navigate = useNavigate(); // Use the useNavigate hook
+
+  return (
     <div>
-      <input type="text" placeholder='username' name="" id="" />
+      <input type="text" placeholder="username" />
       <br />
-      <input type="password" placeholder='password' />
+      <input type="password" placeholder="password" />
       <br />
+      <button onClick={() => navigate('/dashboard')}>Dashboard</button> {/* Fixed navigate */}
       <button onClick={() => loginWithRedirect()}>Submit</button>
     </div>
-  )
+  );
 }
